1120e22d4cec8323a1451ab3887b897baf32d626
Ayush <ayushagarwal.dev@gmail.com>
2023-07-26 01:13:36 +0530

    some fixes

--- src/script.js
+++ src/script.js
@@ -11,15 +11,13 @@ const soundOffSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="
 
 const soundOnSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 17h-5v-10h5v10zm2-10v10l9 5v-20l-9 5zm11.008 2.093c.742.743 1.2 1.77 1.198 2.903-.002 1.133-.462 2.158-1.205 2.9l1.219 1.223c1.057-1.053 1.712-2.511 1.715-4.121.002-1.611-.648-3.068-1.702-4.125l-1.225 1.22zm2.142-2.135c1.288 1.292 2.082 3.073 2.079 5.041s-.804 3.75-2.096 5.039l1.25 1.254c1.612-1.608 2.613-3.834 2.616-6.291.005-2.457-.986-4.681-2.595-6.293l-1.254 1.25z"/></svg>`;
 
-window.addEventListener("load", (event) => {
-  audio.play();
-});
-
-btn.addEventListener("click", ()=> {
-  if (audio.duration > 0 && !audio.paused) {
+btn.addEventListener("click", () => {
+  if (!audio.paused) {
     audio.pause();
     btn.innerHTML = soundOnSvg;
+    console.log(audio.paused);
   } else {
+    console.log(audio.paused);
     audio.play();
     btn.innerHTML = soundOffSvg;
   }
@@ -30,6 +28,7 @@ btn.addEventListener("click", ()=> {
  */
 // Debug
 const gui = new dat.GUI();
+gui.hide();
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -98,7 +97,7 @@ const moonAmbientOcclusionTexture = textureLoader.load(
   "/textures/moon/ambientOcclusion.jpg"
 );
 const moonNormalTexture = textureLoader.load("/textures/moon/normal.jpg");
-const moonRoughnessTexture = textureLoader.load("/textures/moon/roughness.jpg");
+const moonRoughnessTexture = textureLoader.load("/textures/moon/roughness.png");
 
 // Stone Texture
 const stoneColorTexture = textureLoader.load("/textures/stone/color.jpg");
@@ -344,7 +343,7 @@ const moonMaterial = new THREE.MeshBasicMaterial({
   map: moonColorTexture,
   aoMap: moonAmbientOcclusionTexture,
   normalMap: moonNormalTexture,
-  roughnessMap: moonRoughnessTexture,
+  // roughnessMap: moonRoughnessTexture,
 });
 const moon = new THREE.Mesh(moonGeometry, moonMaterial);
 scene.add(moon);
@@ -369,7 +368,7 @@ candle.position.y = 6;
 
 const candleLight = new THREE.PointLight("#561a69", 8, 6);
 candleLight.position.set(0, 8, 3);
-scene.add(candleLight)
+scene.add(candleLight);
 
 // Mud Pathches
 // const mudGeometry = new THREE.CircleGeometry(1, 15);
